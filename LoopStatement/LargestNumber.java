77e6198e2649084cf19e113cce1b44f92b52bf27
manish7739 <manishkumar12118@gmail.com>
2021-09-13 10:03:44 -0400

    update  file

---
+++ LoopStatement/LargestNumber.java
@@ -0,0 +1,31 @@
+package LoopStatement;
+import java.util.Scanner;
+public class LargestNumber {
+	public static void main(String args[]) {
+int number[];
+int size, large;
+Scanner scn=new Scanner(System.in);
+System.out.println("Enter size of array : ");
+size=scn.nextInt();
+number=new int[size];
+System.out.println("enter elements aof array : ");
+for(int i=0; i<number.length; i++) {
+	number[i]=scn.nextInt();
+}
+	large=number[0];
+	
+	for(int i=0; i<number.length;i++) {
+		if(large<number[i]) {
+			large=number[i];
+		}
+	}
+	System.out.println("largest Value is  : " +large );
+	
+
+
+
+
+}
+}
+
+
---
+++ LoopStatement/PrimeNumber.java
@@ -0,0 +1,5 @@
+package LoopStatement;
+
+public class PrimeNumber {
+
+}
