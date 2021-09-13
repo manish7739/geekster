8c55a1c614b03f63ed71f6e19c3a5f6c617de41e
manish7739 <manishkumar12118@gmail.com>
2021-09-13 07:45:54 -0400

    update sum100Number file

--- LoopStatement/Sum100Number.java
+++ LoopStatement/Sum100Number.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 
 public class Sum100Number {
 	public static void main(String args[]) {
-		int t=010;
-		int w=07;
-		System.out.println("Enter the Number as You want to sum of digits : " +w);
 		Scanner s= new Scanner(System.in);
 		System.out.println("Enter the Number as You want to sum of digits : ");
 		int y=s.nextInt();
@@ -17,11 +14,6 @@ public class Sum100Number {
 					
 				}
 		System.out.println("Sum of First 100  Natural Numbers is = " +sum);
-		int t=010;
-		int w=07;
-		System.out.println("Enter the Number as You want to sum of digits : " +w);
-		
-		
 	}
 
 }
